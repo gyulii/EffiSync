6405e4a18a21408fe2e58593a09b997d8fa8b345
Szabó Benedek <benedek.szabo2003@gmail.com>
2025-01-17 00:33:58 +0100

    RelayClient, RelayManager

--- app/network/network_manager.py
+++ app/network/network_manager.py
@@ -11,23 +11,38 @@ class TimetableRecord:
     project: str
     day: datetime.date
     hours: float
-    user: str = "Test User"
 
     def __str__(self):
-        return f"{self.user} - {self.wbs} - {self.project} - {self.day} - {self.hours}"
+        return f"{self.wbs} - {self.project} - {self.day} - {self.hours}"
 
 
-def get_project_data():
-    """Get the timetable records for a specific project."""
-    url = f"https://152.66.182.112:5002/get"
-    response = requests.get(url, headers={'key': 'a'}, verify=False)
-    if response.status_code == 200:
-        records = response.json()
-        return [TimetableRecord(**record) for record in records]
-    else:
-        return []
+class RelayManager:
+    key = None
+    datas = []
+
+    def __init__(self, url: str = "https://152.66.182.112:5002", verify: bool = False):
+        self.url = url
+        self.verify = verify
+
+    def get_data(self):
+        """Get the timetable records for the manager."""
+        response = requests.get(f"{self.url}/get", headers={'key': self.key}, verify=self.verify)
+        if response.status_code == 200:
+            records = response.json()
+            return [TimetableRecord(**record) for record in records]
+        else:
+            return []
+
+    def sync_manager(self, projects: list, topics: list):
+        """Sync the projects with the manager."""
+        response = requests.post(f"{self.url}/sync/manager", data={'projects':projects, 'topics':topics}, headers={'key': self.key}, verify=self.verify)
+        if response.status_code == 200:
+            return True, response.text
+        else:
+            return False, response.text
 
 if __name__ == "__main__":
-    records = get_project_data()
+    manager = RelayManager()
+    records = manager.get_data()
     for record in records:
         print(record)
